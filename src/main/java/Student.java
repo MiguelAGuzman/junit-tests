8c6ca9fea953596ac8f19dce65e80bb33418c52d
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-23 12:05:56 -0500

    j unit test ex started

---
+++ src/main/java/Student.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+    // returns the student's id
+    public long getId(){
+
+    }
+
+    // returns the student's name
+    public String getName(){
+
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+
+    }
+}
