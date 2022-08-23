72af3dc5effc421515dbb147553d35978b47053c
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-23 12:24:26 -0500

    j unit ex added methods

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -6,34 +6,40 @@ public class Student {
     private long id;
     private ArrayList<Integer> grades;
 
-    public Student(String name, long id, ArrayList<Integer> grades) {
+    public Student(String name, long id) {
         this.name = name;
         this.id = id;
-        this.grades = grades;
+        this.grades = new ArrayList<>();
     }
 
     // returns the student's id
     public long getId(){
-
+        return id;
     }
 
     // returns the student's name
     public String getName(){
-
+        return name;
     }
 
     // adds the given grade to the grades list
     public void addGrade(int grade){
-
+        grades.add(grade);
     }
 
     // returns the list of grades
     public ArrayList<Integer> getGrades(){
-
+        return grades;
     }
 
     // returns the average of the students grades
     public double getGradeAverage(){
-
+        double sum = 0;
+        for(Integer i: grades) {
+            sum += i;
+        }
+        return sum / (double) grades.size();
     }
+
+
 }
